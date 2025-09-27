4cb227160b46ceda7adbe6f7d3ac30d2746606a5
Sebastian Tafur <eliottxt650@gmail.com>
2025-09-26 19:29:24 -0500

    arreglando

---
+++ src/test/java/com/example/PROYECTO_MDW/ProyectoMdwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PROYECTO_MDW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoMdwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
