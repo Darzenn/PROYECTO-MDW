2af7c2e2d6f4669ea208d587a32533e7341481f2
Sebastian Tafur <eliottxt650@gmail.com>
2025-09-26 20:23:26 -0500

    controlador

---
+++ src/main/java/com/example/PROYECTO_MDW/Controlador/Controlador.java
@@ -0,0 +1,13 @@
+package com.example.PROYECTO_MDW.Controlador;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Controlador {
+    
+    @GetMapping("/index")
+    public String mostrarIndex() {
+        return "index";
+    }
+}
--- src/main/java/com/example/PROYECTO_MDW/Controlador/Controller.java
+++
@@ -1,5 +0,0 @@
-package com.example.PROYECTO_MDW.Controlador;
-
-public class Controller {
-    // Aquí puedes agregar métodos y lógica para controlar la aplicación
-}
