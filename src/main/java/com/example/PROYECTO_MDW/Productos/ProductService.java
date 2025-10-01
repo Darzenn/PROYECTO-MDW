c91b698c46f74384af56d228c25c1f4108d52cc6
ltsyy18 <dayanaacho04@gmail.com>
2025-09-30 22:36:25 -0500

    arreglando zonapago

--- src/main/java/com/example/PROYECTO_MDW/Productos/ProductService.java
+++ src/main/java/com/example/PROYECTO_MDW/Productos/ProductService.java
@@ -8,15 +8,15 @@ import java.util.List;
 public class ProductService {
 
     private static final List<Product> products = Arrays.asList(
-        new Product(101, "Reloj Deportivo", 50.00, 15),
+        new Product(101, "Reloj Deportivo", 50.00, 5),
         new Product(102, "Reloj Clásico", 80.00, 5),
-        new Product(103, "Reloj Inteligente", 150.00, 25),
-        new Product(104, "Reloj con Correa de Acero", 120.00, 10),
-        new Product(105, "Reloj de Muñeca", 150.00, 8),
-        new Product(106, "Reloj Digital", 300.00, 2),
-        new Product(107, "Reloj para Damas", 700.00, 20),
-        new Product(108, "Reloj Deportivo Femenino", 600.00, 12),
-        new Product(109, "Reloj Porta Auriculares", 849.00, 4)
+        new Product(103, "Reloj Inteligente", 150.00, 5),
+        new Product(104, "Reloj con Correa de Acero", 120.00, 5),
+        new Product(105, "Reloj de Muñeca", 150.00, 5),
+        new Product(106, "Reloj Digital", 300.00, 5),
+        new Product(107, "Reloj para Damas", 700.00, 5),
+        new Product(108, "Reloj Deportivo Femenino", 600.00, 5),
+        new Product(109, "Reloj Porta Auriculares", 849.00, 5)
     );
 
     public List<Product> getAllProducts() {
